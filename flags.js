d92b1ec22e1791d8af499612a034d891d8421300
Eugeny2010 <125874750+Eugeny2010@users.noreply.github.com>
2023-04-28 23:09:46 +0300

    Add files via upload

---
+++ flags.js
@@ -0,0 +1,14 @@
+//var id = '<img src="flags/" height="200px" width="300px"/>';
+
+var asri = '<img src="flags/asr.jpg" height="200px" width="300px"/>';
+var ruri = '<img src="flags/rur.jpg" height="200px" width="300px"/>';
+var voli = '<img src="flags/vol.jpg" height="200px" width="300px"/>';
+var liti = '<img src="flags/lit.jpg" height="200px" width="300px"/>';
+var rski = '<img src="flags/rsk.jpg" height="200px" width="300px"/>';
+var sevi = '<img src="flags/sev.jpg" height="200px" width="300px"/>';
+var reti = '<img src="flags/ret.jpg" height="200px" width="300px"/>';
+var slai = '<img src="flags/sla.jpg" height="200px" width="300px"/>';
+var ruli = '<img src="flags/rul.jpg" height="200px" width="300px"/>';
+var kapi = '<img src="flags/kap.jpg" height="200px" width="300px"/>';
+var akri = '<img src="flags/akr.jpg" height="200px" width="300px"/>';
+var huri = '<img src="https://sun9-68.userapi.com/impg/lPwOVbp3M6NNAFnzYuZpgVcuIo56uvARHbXpRw/8YocsHp6fC4.jpg?size=590x393&quality=95&sign=ef7826f76d86dfd9cdc69da060d4217f&type=album" height="200px" width="300px"/>';
